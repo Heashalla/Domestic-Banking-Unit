4c61d3f66575c3c0407ce8f3faa034d4021d3095
Heashalla <heashallabaanusundaresan@gmail.com>
2025-04-28 18:10:39 +0530

    test 116

--- Visualisation.py
+++ Visualisation.py
@@ -20,8 +20,8 @@ def sri_lanka_flag_background():
                 135deg,
                 #8D1B1B 20%,
                 #007847 50%,
-                #FF8200 50%,
-            );
+                #F5D300 75%,
+                #D1C6B1 100%
             background-size: 400% 400%;
             animation: gradientAnimation 15s ease infinite;
         }
